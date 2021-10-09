1f297ee416525cd833b516b0ccb9ea435cdfacc4
L0neGamer <benjaminbwm@gmail.com>
2021-10-09 11:30:47 +0100

    added some additional instances of Parsable due to seeing finnbar's ones and being jealous

--- src/Tablebot/Plugin/SmartCommand.hs
+++ src/Tablebot/Plugin/SmartCommand.hs
@@ -11,32 +11,49 @@
 -- Let's find out
 module Tablebot.Plugin.SmartCommand where
 
--- TODO: Much helpful functionality is missing here.
-import Tablebot.Plugin.Parser ( untilSpace, integer )
-import Tablebot.Plugin.Types ( DatabaseDiscord, Parser )
-import Discord.Types ( Message )
+import Control.Applicative (optional)
+import Data.Text (Text, pack)
+import Discord.Types (Message)
+import Tablebot.Plugin.Parser (integer, quoted, untilSpace)
+import Tablebot.Plugin.Types (DatabaseDiscord, Parser)
+import Text.Megaparsec (MonadParsec (try), many)
 
 -- | marks that a type can be parsed
 class Parsable t where
-    par :: Parser t
+  par :: Parser t
 
 -- | parsing implementation for all integral types
 -- Overlappable due to the really flexible head state
 instance {-# OVERLAPPABLE #-} (Integral a, Read a) => Parsable a where
-    par = integer 
+  par = integer
 
 -- | parsing implementation for all single words (all characters)
 instance Parsable String where
-    par = untilSpace
+  par = untilSpace
+
+newtype Quoted = Qu Text
+
+-- | stolen from finnbar's implementation
+-- get a quoted string
+instance Parsable Quoted where
+  par = Qu . pack <$> quoted
+
+-- | get a value that might or might not exist
+instance (Parsable a) => Parsable (Maybe a) where
+  par = optional $ try par
+
+-- | get many instances of a value
+instance {-# OVERLAPPABLE #-} (Parsable a) => Parsable [a] where
+  par = many par
 
 -- | make the required command parser
 class MakeCommand xs where
-    parseMK :: xs -> Parser (Message -> DatabaseDiscord ())
+  parseMK :: xs -> Parser (Message -> DatabaseDiscord ())
 
 -- | make the base case for the command parser
 -- Overlapping as it's the most important case
 instance {-# OVERLAPPING #-} MakeCommand (Message -> DatabaseDiscord ()) where
-    parseMK comm = return comm
+  parseMK comm = return comm
 
 -- | make the recursive case for the command parser
 -- Overlappable as we prefer the other case
