04af9c5a7f960c09e2b1f0f512bd20d4957988b3
L0neGamer <benjaminbwm@gmail.com>
2022-02-02 23:56:01 +0000

    added replicate function and set value function

--- src/Tablebot/Plugins/Roll/Dice/DiceFunctions.hs
+++ src/Tablebot/Plugins/Roll/Dice/DiceFunctions.hs
@@ -21,7 +21,7 @@ module Tablebot.Plugins.Roll.Dice.DiceFunctions
 where
 
 import Control.Monad.Exception (MonadException)
-import Data.List (genericDrop, genericLength, genericTake, sort)
+import Data.List (genericDrop, genericLength, genericReplicate, genericTake, sort)
 import Data.Map as M (Map, fromList, keys)
 import Data.Maybe (fromJust)
 import Data.Text (Text, unpack)
@@ -76,6 +76,8 @@ listFunctionsList = M.keys listFunctions
 -- each function that returns an integer.
 listFunctions' :: [FuncInfoBase [Integer]]
 listFunctions' =
+  constructFuncInfo "replicate" (genericReplicate @Integer) :
+  funcInfoSet :
   constructFuncInfo "between" between :
   constructFuncInfo "drop" (genericDrop @Integer) :
   constructFuncInfo "take" (genericTake @Integer) :
@@ -90,7 +92,15 @@ funcInfoIndex = FuncInfo "index" [ATInteger, ATIntegerList] ATInteger fiIndex
     fiIndex (LIInteger i : [LIList is])
       | i < 0 || i >= genericLength is = throwBot $ EvaluationException ("index out of range: " ++ show i) []
       | otherwise = return (is !! fromInteger i)
-    fiIndex is = throwBot $ EvaluationException ("incorrect number of arguments. expected 2, got " ++ show (length is)) []
+    fiIndex is = throwBot $ EvaluationException ("incorrect number/type of arguments. expected 2, got " ++ show (length is)) []
+
+funcInfoSet :: FuncInfoBase [Integer]
+funcInfoSet = FuncInfo "set" [ATInteger, ATInteger, ATIntegerList] ATIntegerList fiSet
+  where
+    fiSet (LIInteger i : LIInteger j : [LIList js])
+      | i < 0 || i >= genericLength js = throwBot $ EvaluationException ("index out of range: " ++ show i) []
+      | otherwise = return $ genericTake i js ++ j : genericDrop (i + 1) js
+    fiSet is = throwBot $ EvaluationException ("incorrect number/type of arguments. expected 3, got " ++ show (length is)) []
 
 -- | A data structure to contain the information about a given function,
 -- including types, the function name, and the function itself.
